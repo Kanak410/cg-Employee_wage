7328366e83a2a925b2ea5a29a0969b028a4d4bae
Kanak Rajput <rajputkanak078@gmail.com>
2025-03-03 22:00:22 +0530

    [kanak]Add. added function to calculate working hours

--- EmployeePresence.js
+++ EmployeePresence.js
@@ -13,17 +13,37 @@ const Is_Full_time=2;
 const Part_hours=4;
 const Full_hours=8;
 const Wage_per_hours=20;
-let empHrs=0;
-empcheck=Math.floor(Math.random()*10)%3;
-switch(empcheck){
-    case Is_Part_time:
-    empHrs=Part_hours;
-    break;
-    case Is_Full_time:
-        empHrs=Full_hours;
-        break;
-    default:
-        empHrs=0;
+//let empHrs=0;
+// empcheck=Math.floor(Math.random()*10)%3;
+// switch(empcheck){
+//     case Is_Part_time:
+//     empHrs=Part_hours;
+//     break;
+//     case Is_Full_time:
+//         empHrs=Full_hours;
+//         break;
+//     default:
+//         empHrs=0;
+// }
+// let empWage=empHrs*Wage_per_hours;
+// console.log("Emp Wage: "+empWage)
+
+
+// uc3- working hours
+function getWorkingHours(empcheck){
+    switch(empcheck){
+        case Is_Part_time:
+            return Part_hours
+        case Is_Full_time:
+            return Full_hours
+        default:
+            return 0;
+    }
+    let empHrs=0;
+    empcheck=Math.floor(Math.random()*10)%3;
+    empHrs=getWorkingHours(empcheck);
+    let empWage=empHrs*Wage_per_hours;
+    console.log("Emp wage: "+empWage);
+    
 }
-let empWage=empHrs*Wage_per_hours;
-console.log("Emp Wage: "+empWage)
+
