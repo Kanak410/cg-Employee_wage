2b2e3412fe085eb18390400e44c0449429ff9286
Kanak Rajput <rajputkanak078@gmail.com>
2025-03-06 01:35:25 +0530

    [kanak]Add. Added Name checker using regex

--- EmployeePresence.js
+++ EmployeePresence.js
@@ -217,16 +217,21 @@ class EmployeePayrollData{
     startDate;
     constructor(...params){
         this.id=params[0];
-        this.salary=params[1];
-        this.name=params[2];
-        this.grnder=params[3];
+        this.salary=params[2];
+        this._name=params[1];
+        this.gender=params[3];
         this.startDate=params[4];
     }
     get name(){
         return this._name;
     }
     set name(name){
-        this._name=name
+       let nameRegex=RegExp("^[A-Z]{1}[a-z]{3,}$");
+       if(nameRegex.test(name)){
+        this._name=name;
+       }else{
+         throw new Error("Name is incorrect!!");
+       }
     }
     toString(){
         const options={year: 'numeric',month:'long',day:'numeric'};
@@ -235,11 +240,13 @@ class EmployeePayrollData{
         return "id="+this.id+", name"+this.name+",salary="+this.salary+","+"gender="+this.gender+", startDate="+empDate;
     }
 }
-let employeePayrollData=new EmployeePayrollData(1,"kanak",2000)
+let employeePayrollData=new EmployeePayrollData(1,"Kanak",2000)
 console.log(employeePayrollData.toString())
-employeePayrollData.name="kanak"
+try{
+employeePayrollData.name="Kanak"
 console.log(employeePayrollData.toString()); 
+}catch(e){
+    console.error(e)
+}
 let newEmployeePayrollData=new EmployeePayrollData(2,"payal",40000,"female",new Date(2002,4,10))
 console.log(newEmployeePayrollData.toString())
-
-
